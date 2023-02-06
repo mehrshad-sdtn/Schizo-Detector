d05bf52357066d1fb5b3f8ed92340b3892a17a4b
Mehrshad Saadatinia <saadatiniamehrshad@gmail.com>
2023-02-06 19:17:37 +0330

    app changed

--- app.py
+++ app.py
@@ -1,43 +1,83 @@
 from flask import Flask, render_template, request
+import tensorflow as tf
+import eeg_processing as eegp
+import image_utils
 import os
-import numpy as np
-import classifier as clf
-import sys
 import logging
-import PIL
+import numpy as np
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
-app.config['UPLOAD_FOLDER'] =os.path.join('static', 'images')
+model = model = tf.keras.models.load_model('simple_model_final.h5')
 
-def build_and_save_image(image):
-    data = image.read()
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-    with open(file_path, 'wb') as file:
-        file.write(data)
-
-def clean_image_dir():
-    for im in os.listdir(app.config['UPLOAD_FOLDER']):
-        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], im))
+def clear_directory(dir_path):
+    files = os.listdir(dir_path)
+    for f in files:
+        os.remove(os.path.join(dir_path, f))
 
 
-@app.route("/")
-def hello():
-    return render_template("index.html")
+def diagnose(zeros, ones):
+    percent_sch = round((ones/12)*100, 1)
+    print(percent_sch)
+    if percent_sch > 60 and percent_sch < 90:
+        return f"You are at a very high ({percent_sch}%) risk of having Schizophrenia, please consult a professional for furthur diagnosis and assistance as soon as possible"
 
-@app.route("/sub", methods = ['POST'])
-def submit():
-    predictions = []
-    clean_image_dir()
-    if request.method == "POST":
-        image = request.files["file"]
-        build_and_save_image(image)
-        im_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-        pred = clf.get_image_prediction(im_path)
+    elif percent_sch > 90:
+        return f"You are diagnosed with Schizophrenia (with {percent_sch}% confidence), please seek a professional's help for your treatment options as soon as possible"
 
-        return render_template('sub.html', name = pred, image=im_path)
+    elif percent_sch < 60 and percent_sch > 40:
+        return f"You are at a the ({percent_sch}%) risk of having Schizophrenia, please consult a professional for furthur diagnosis and assistance"
+
+    elif percent_sch < 40 and percent_sch > 10:
+        return f"You probably do not have Schizophrenia, But still have a {percent_sch}% risk, please also consult a professional"
+
+    else:
+        return f"You are healthy ({percent_sch}% chance of having Schizophrenia)"
+
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    clear_directory('EEG')
+    clear_directory('images')
+    return render_template('index.html')
+
+
+@app.route('/diagnosis', methods=['POST'])
+def predict():
+    eegfile=request.files['eegfile']
+    eegpath = os.path.join("EEG", eegfile.filename)
+    eegfile.save(eegpath)
+    eegp.process_eeg_data()
+    X = image_utils.image_arrays_from_directory('images')
+    y_preds = model.predict(X)
+    predictions = np.argmax(y_preds, axis=-1)
+    unique, counts = np.unique(predictions, return_counts=True)
+    dictionary = dict(zip(unique, counts))
+
+    zeros, ones = 0, 0
+    if 0 in dictionary:
+        zeros = dictionary[0]    
+    else:
+        zeros = 0
+    
+    if 1 in dictionary:
+        ones = dictionary[1]
+    else:
+        ones = 0
+
+    print(zeros, ones)
+    diagnosis = diagnose(zeros, ones)
+    print(diagnosis)
+    return render_template('sub.html', text=diagnosis)
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run() 
---
+++ eeg_processing.py
@@ -0,0 +1,110 @@
+import os
+import math
+import random
+import numpy as np
+from matplotlib import gridspec
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+recording_len = 7680
+freq = 128
+total_time = recording_len // freq
+
+def read_data(path):
+  case_path = path
+  subjects_list = []
+  for filename in os.listdir(case_path):
+    with open(os.path.join(case_path, filename), 'r') as f:
+      lines = f.readlines()
+      subjects_list.append(lines)
+  return subjects_list
+
+# -----------------------------------------
+def convert_to_numbers(sub_list):
+  new_list = []
+  for sub in sub_list:
+    new_list.append([float(value) for value in sub])
+  return new_list
+
+# -----------------------------------------
+def list_segments(lst, segments=16):
+  arr = np.array(lst)
+  return np.array_split(np.array(arr), segments)
+
+
+# --------------------------------------------
+def divide_to_channels(subjects_list, segments=16):
+  return [list_segments(subject, segments) for subject in subjects_list]
+
+
+# ----------------------------------------------
+def log_data_shapes(data):
+  return {
+    (len(data), len(data[0]), data[0][0].shape)}
+
+# --------------------------------------------------
+def divide_time_segments(subject_list, time_window=5):
+  n_segments = total_time // time_window
+  return [np.split(channel, n_segments) for channel in subject_list]
+
+
+# -----------------------------------------------
+def create_spectrogram_data(subject_list):
+  new_subject_list = []
+  for subject in subject_list:
+    specs = []
+    segment_channels = divide_time_segments(subject)
+    array_of_arrays = np.array([np.array(channel) for channel in segment_channels])
+    for i in range(0, array_of_arrays.shape[1]):
+      spec_data = np.squeeze(array_of_arrays[:, i, :].reshape((1, -1)))
+      specs.append(spec_data)
+    new_subject_list.append(specs)
+  return new_subject_list
+
+
+###############################################################################
+channels_16 = ['F7', 'F3', 'F4', 'F8', 'T3', 'C3', 'Cz', 'C4', 
+               'T4', 'T5', 'P3', 'Pz', 'P4', 'T6', 'O1', 'O2']
+
+
+def create_spectrogram(data, fs, impath= '', save=False):
+  plt.specgram(data, Fs=fs, NFFT=1024)
+  if save:
+    plt.axis('off')
+    plt.savefig(impath, dpi=300, pad_inches=0.0, transparent=True, bbox_inches='tight')
+  else: plt.show()
+
+
+
+def generate_images_for_subject(path, subject_data, sub_index):
+  for i, segment in enumerate(subject_data):
+    filename = f'sub{sub_index}-seg{i}'
+    print('generated: ', filename)
+    create_spectrogram(segment, fs=freq, impath=os.path.join(path, filename), save=True)
+
+
+
+def generate_images(data, path, start=0):
+  for i in range(start, len(data)):
+    generate_images_for_subject(path, data[i], i)
+
+
+#################################| main |#####################################
+
+def process_eeg_data():
+    """
+    This function reads the uploaded EEG from directory and 
+    creates corresponding spectrogram images of 5 second segments
+
+    """
+    print(f'the recording is {total_time} seconds long and has total {recording_len} values')
+
+
+    raw_data = convert_to_numbers(read_data('./EEG'))
+    by_channels = divide_to_channels(raw_data)
+    spec_data = create_spectrogram_data(by_channels)
+    generate_images(spec_data, './images')
+
+
